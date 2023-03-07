2c6c22babd44ecc474fd8e9c294d1dbfa5041c9e
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-03-07 21:05:21 +0100

    Implement Schema and install bcrypt

---
+++ src/database/models/User.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+export default User;
