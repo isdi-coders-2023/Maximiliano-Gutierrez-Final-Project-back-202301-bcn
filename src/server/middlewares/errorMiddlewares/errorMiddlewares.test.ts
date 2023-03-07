5f3d60e66a943d9bfa763d7e325e24f4df7a86e9
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-03-07 17:37:28 +0100

    Implement error middleware and test

---
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.test.ts
@@ -0,0 +1,40 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../../CustomError/CustomError";
+import { generalError, notFoundError } from "./errorMiddlewares";
+
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+} as Partial<Response>;
+
+const req = {} as Request;
+const next = jest.fn() as NextFunction;
+
+beforeEach(() => jest.clearAllMocks());
+
+describe("Given a generalError middleware", () => {
+  describe("When it receives an erro with status 500", () => {
+    test("Then it sould call its status method with a 500", () => {
+      const statusCode = 500;
+      const error = new CustomError(
+        "There was an error",
+        500,
+        "Something went wrong"
+      );
+
+      generalError(error, req, res as Response, next);
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+  });
+
+  describe("Given a notFoundError middleware", () => {
+    describe("When it receives a request", () => {
+      describe("Then it should call its next method", () => {
+        notFoundError(req, res as Response, next);
+
+        expect(next).toHaveBeenCalled();
+      });
+    });
+  });
+});
