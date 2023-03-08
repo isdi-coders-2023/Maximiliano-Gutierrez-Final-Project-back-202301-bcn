bd6a8c9863bde968107226f2154aa9891729a8ef
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-03-08 10:46:59 +0100

    Create UsersRouter for login and schemas for login

---
+++ src/server/routers/usersRouters/usersRouters.ts
@@ -0,0 +1,17 @@
+import Router from "express";
+import { validate } from "express-validation";
+import { loginUser } from "../../controllers/usersControllers/usersControllers.js";
+import routes from "../routes.js";
+import loginUserSchema from "../../schemas/loginUserSchema.js";
+
+const { login } = routes.users;
+
+const usersRouter = Router();
+
+usersRouter.post(
+  login,
+  validate(loginUserSchema, {}, { abortEarly: false }),
+  loginUser
+);
+
+export default usersRouter;
