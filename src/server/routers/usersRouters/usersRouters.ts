8223d01af7270fd5411e4388c68b02388db45759
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-03-09 16:48:01 +0100

    Add cors.ts file in server folder

--- src/server/routers/usersRouters/usersRouters.ts
+++ src/server/routers/usersRouters/usersRouters.ts
@@ -1,4 +1,4 @@
-import Router from "express";
+import { Router } from "express";
 import { validate } from "express-validation";
 import { loginUser } from "../../controllers/usersControllers/usersControllers.js";
 import routes from "../routes.js";
